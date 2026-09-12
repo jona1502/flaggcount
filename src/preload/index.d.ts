8162780197565dad89b922ebc23a32e3542d5233
jona1502 <muhr999j@gmail.com>
2026-09-13 00:03:15 +0200

    Revert "chore: initialize Electron React TypeScript project"
    This reverts commit 6d98d20b1e4c16190579dec97555dcc407396e1a.

--- src/preload/index.d.ts
+++
@@ -1,7 +0,0 @@
-import type { FlagCountApi } from './index';
-
-declare global {
-  interface Window {
-    flagcount: FlagCountApi;
-  }
-}
--- src/preload/index.ts
+++
@@ -1,9 +0,0 @@
-import { contextBridge } from 'electron';
-
-const api = {
-  platform: process.platform
-};
-
-export type FlagCountApi = typeof api;
-
-contextBridge.exposeInMainWorld('flagcount', api);
